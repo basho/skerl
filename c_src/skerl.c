0bdd076cdbdca2a0f35b40329b0cbcfb39caef24
b <b@b3k.us>
2010-05-04 13:11:33 -0700

    Initial commit.

---
+++ c_src/skerl.c
@@ -0,0 +1,109 @@
+
+#include "erl_nif.h"
+#include "skein_api.h"
+
+static ErlNifResourceType* skein_RESOURCE;
+static ErlNifResourceType* skein_hashstate;
+static ErlNifResourceType* skein_hashval;
+
+typedef struct
+{
+} skein_handle;
+
+// Prototypes
+ERL_NIF_TERM skein_init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+ERL_NIF_TERM skein_update(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+ERL_NIF_TERM skein_final(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+ERL_NIF_TERM skein_hash(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+
+static ErlNifFunc nif_funcs[] =
+{
+    {"init", 1, skein_init},
+    {"update", 2, skein_update},
+    {"final", 1, skein_final},
+    {"hash", 2, skein_hash}
+};
+
+
+ERL_NIF_TERM skein_init(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{	
+	int bits = 0;
+	enif_get_int(env, argv[0], &bits);
+	
+	hashState *state = enif_alloc_resource(env, skein_hashstate, sizeof(hashState));
+	HashReturn r = Init(state, bits);
+	if (r == SUCCESS) {
+		ERL_NIF_TERM result = enif_make_resource(env, state);
+	    enif_release_resource(env, state);
+	    return enif_make_tuple2(env, enif_make_atom(env, "ok"), state);
+	} else {
+		return enif_make_tuple2(env, enif_make_atom(env, "error"), r);		
+	}
+}
+
+ERL_NIF_TERM skein_update(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+	hashState *state = NULL;
+	enif_get_resource(env, argv[0], skein_hashstate, (void**)&state);
+
+	ErlNifBinary bin;
+	enif_inspect_binary(env, argv[1], &bin);
+	
+	HashReturn r = Update(state, bin.data, bin.size * 8);
+	
+    return enif_make_atom(env, "ok");
+}
+
+ERL_NIF_TERM skein_final(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+	hashState *state = NULL;
+	enif_get_resource(env, argv[0], skein_hashstate, (void**)&state);
+	
+	BitSequence *hash = enif_alloc_resource(env, skein_hashval, sizeof(state->statebits / 8));;
+	HashReturn r = Final(state, hash);
+	if (r == SUCCESS) {
+		ERL_NIF_TERM result = enif_make_resource(env, hash);
+	    enif_release_resource(env, hash);
+	    return enif_make_tuple2(env, enif_make_atom(env, "ok"), hash);
+	} else {
+		free(hash);
+		return enif_make_tuple2(env, enif_make_atom(env, "error"), r);		
+	}
+}
+
+ERL_NIF_TERM skein_hash(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+	int bits = 0;
+	enif_get_int(env, argv[0], &bits);
+	
+	ErlNifBinary bin;
+	enif_inspect_binary(env, argv[1], &bin);
+	
+	BitSequence *hash = enif_alloc_resource(env, skein_hashval, sizeof(bits / 8));;
+	HashReturn r = Hash(bits, (BitSequence *)(bin.data), bin.size * 8, hash);
+	if (r == SUCCESS) {
+		ERL_NIF_TERM result = enif_make_resource(env, hash);
+	    enif_release_resource(env, hash);
+	    return enif_make_tuple2(env, enif_make_atom(env, "ok"), hash);
+	} else {
+		free(hash);
+		return enif_make_tuple2(env, enif_make_atom(env, "error"), r);		
+	}	
+}
+
+static void skein_resource_cleanup(ErlNifEnv* env, void* arg)
+{
+    // Delete any dynamically allocated memory stored in skein_handle
+    // skein_handle* handle = (skein_handle*)arg;
+}
+
+static int on_load(ErlNifEnv* env, void** priv_data, ERL_NIF_TERM load_info)
+{
+    skein_RESOURCE = enif_open_resource_type(env, "skein_resource",
+                                                  &skein_resource_cleanup,
+                                                  ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER,
+                                                  0);
+    return 0;
+}
+
+ERL_NIF_INIT(skein, nif_funcs, &on_load, NULL, NULL, NULL);
---
+++ src/skerl.erl
@@ -0,0 +1,44 @@
+-module(skerl).
+
+-export([init/2,
+		 update/3,
+		 final/2,
+         hash/3]).
+
+-on_load(init/0).
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+init() ->
+    case code:priv_dir(skerl) of
+        {error, bad_name} ->
+            SoName = filename:join("../priv", skerl);
+        Dir ->
+            SoName = filename:join(Dir, skerl)
+    end,
+    erlang:load_nif(SoName, 0).
+
+init(Ref, Bits) ->
+	"NIF library not loaded".
+
+update(Ref, State, Data) ->
+	"NIF library not loaded".
+
+final(Ref, State) ->
+	"NIF library not loaded".
+	
+hash(Ref, Bits, Data) ->
+    "NIF library not loaded".
+
+%% ===================================================================
+%% EUnit tests
+%% ===================================================================
+-ifdef(TEST).
+
+basic_test() ->
+    {ok, Ref} = new(),
+    ok = init(Ref).
+
+-endif.
